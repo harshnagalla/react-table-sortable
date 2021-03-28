04c23a2fe66ffdbb9b856b66a20939c07c265905
Harsh Nagalla <harshnagalla@u.nus.edu>
2021-03-28 11:55:04 +0800

    Add Base files

---
+++ src/components/Table.jsx
@@ -0,0 +1,58 @@
+import { useSortableData } from "../hooks/useSortableData";
+
+const Table = (props) => {
+  const { items, requestSort, sortConfig } = useSortableData(props.products);
+  const getClassNamesFor = (name) => {
+    if (!sortConfig) {
+      return;
+    }
+    return sortConfig.key === name ? sortConfig.direction : undefined;
+  };
+  return (
+    <table>
+      <caption>Products</caption>
+      <thead>
+        <tr>
+          <th>
+            <button
+              type="button"
+              onClick={() => requestSort("name")}
+              className={getClassNamesFor("name")}
+            >
+              Name
+            </button>
+          </th>
+          <th>
+            <button
+              type="button"
+              onClick={() => requestSort("price")}
+              className={getClassNamesFor("price")}
+            >
+              Price
+            </button>
+          </th>
+          <th>
+            <button
+              type="button"
+              onClick={() => requestSort("stock")}
+              className={getClassNamesFor("stock")}
+            >
+              In Stock
+            </button>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map((item) => (
+          <tr key={item.id}>
+            <td>{item.name}</td>
+            <td>${item.price}</td>
+            <td>{item.stock}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
